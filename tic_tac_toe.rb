73494921761022ff9c77f1d4648ab3ecf402ac0e
Katrin Marschner <katrin.marschner@gmail.com>
2015-05-16 23:39:01 +0200

    add class Game in TicTacToe

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,5 @@
 class Grid
-  def row(content = [" ", " ", " "])
+  def row(content)
     content.map! { |cell| " #{cell} " }
     content.join("|")
   end
@@ -20,51 +20,67 @@ class Grid
   end
 end
 
-def winner(cells)
-  wins = [
-    [1,2,3],[4,5,6],[7,8,9],
-    [1,4,7],[2,5,8],[3,6,9],
-    [1,5,9],[3,5,7]
-  ]
+class Game
+  attr_reader :grid
+  def initialize
+    @grid = Grid.new
+    @cells = [1,2,3,4,5,6,7,8,9]
+    @player = [1,2]
+    @xo = ["o", "x"]
+  end
 
-  wins.map! do |win|
-    win.map! do |position|
-      cells[position-1]
+  def winner?
+    wins = [
+      [1,2,3],[4,5,6],[7,8,9],
+      [1,4,7],[2,5,8],[3,6,9],
+      [1,5,9],[3,5,7]
+    ]
+
+    wins.map! do |win|
+      win.map! do |position|
+        @cells[position-1]
+      end
+    end
+    
+    if wins.include?(["x","x","x"]) || wins.include?(["o","o","o"])
+      true
+    else
+      false
     end
   end
-  
-  if wins.include?(["x","x","x"]) || wins.include?(["o","o","o"])
-    true
-  else
-    false
+
+  def ending?
+    @cells.uniq == ["o", "x"]
   end
-end
 
-cells = [1,2,3,4,5,6,7,8,9]
-
-grid = Grid.new
-
-player = [1,2]
-xo = ["o", "x"]
-
-until winner(cells) || cells.uniq == ["o", "x"]
-  puts grid.format(cells)
-  print "input Player #{player[0]}: "
-  input = gets.chomp
-  while cells[input.to_i - 1] == "o" || cells[input.to_i - 1] == "x"
-    puts "input not valid, new input:"
+  def play
+    puts grid.format(@cells)
+    print "input Player #{@player[0]}: "
     input = gets.chomp
+    while @cells[input.to_i - 1] == "o" || @cells[input.to_i - 1] == "x"
+      puts "input not valid, new input:"
+      input = gets.chomp
+    end
+    @cells[input.to_i - 1] = "#{@xo[0]}"
+
+    @player.reverse!
+    @xo.reverse!
   end
-  cells[input.to_i - 1] = "#{xo[0]}"
 
-  player.reverse!
-  xo.reverse!
+  def final_output
+    puts grid.format(@cells)
+    if winner?
+      puts "Player #{@player[1]} wins."
+    elsif ending?
+      puts "No possibilities left. No winner."
+    end
+  end
 end
 
-puts grid.format(cells)
+game = Game.new
 
-if winner(cells)
-  puts "Player #{player[1]} wins."
-elsif cells.uniq == ["o", "x"]
-  puts "No possibilities left. No winner."
+until game.winner? || game.ending?
+  game.play
 end
+
+game.final_output
